86f920d251afa0a53d8861a396e4e4fb94ab6e18
Farfadeli <ndaunac@gmail.com>
2024-05-21 16:00:46 +0200

    ab

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import additionner
 
 class CalculMethod(unittest.TestCase):
     def test_additionner(self):
-        self.assertEqual(additionner(2, 3) , 4)
+        self.assertEqual(additionner(2, 3) , 5)
 
 if __name__ == '__main__':
     unittest.main()
